9132cd198e57e776e9b21afb83900bc780c24d87
Reginatia Amoah <reginatiaamoah@gmail.com>
2025-11-24 22:36:51 +0000

    Initial Python

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-print ("Hello World !")
-print ("*" * 10)
+print("Hello World !")
+print("*" * 10)
 # this is to multiply x ten times.
-2 + 1 
+2 + 1
+
+# declaring a variables. this is done using formatting which is autopep8
+x = 1
+
+x = 2
+x = 5
+
+# example of an expression
+"*" * 3
