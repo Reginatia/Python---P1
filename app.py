efa54617026a14ec50b37ca6e4c304f13b106662
Reginatia Amoah <reginatiaamoah@gmail.com>
2025-11-25 12:22:04 +0000

    Python

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 print("Hello World !")
 print("*" * 10)
 # this is to multiply x ten times.
-2 + 1
+(2 + 1)
 
 # declaring a variables. this is done using formatting which is autopep8
 x = 1
@@ -10,4 +10,14 @@ x = 2
 x = 5
 
 # example of an expression
-"*" * 3
+print("*" * 3)
+
+# We have primetive types of data,
+# Boolean (true, false), string (""), and numbers (integers and float)
+students_count = 100
+rating = 3.9
+published_today = True
+course_name = "Python Programming"
+
+# one thing to know about python is that it is case sensitive, so always use lower case letter,
+# and to make code understandable, it is only underscore that can be used to separate variables (_)
